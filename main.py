825bb475e915161dfac528a038687a2bd7385d10
FelipeDaniel <felipedaniel1910@hotmail.com>
2021-04-29 00:11:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,24 +17,28 @@ class MoodleBot:
     def login(self):
         username = self.driver.find_element_by_id("username")
         time.sleep(3)
-        username.send_keys('')
+        username.send_keys('') #CAMPO DE LOGIN
         password = self.driver.find_element_by_id("password")
-        password.send_keys('' + Keys.RETURN)
+        password.send_keys('' + Keys.RETURN) #CAMPO DE SENHA
         time.sleep(5)
 
     #Função que abre a disciplina
     def subject_enter(self):
-        subj_titles = ['"ET39H.2020_02 - Robotica - A81 (2020_02)"',
+        subj_titles = ['felipe daniel',
+        '"ET39H.2020_02 - Robotica - A81 (2020_02)"',
         '"ET68B - Oficina de Integração 2 - A81 (2020_02)"',
         '"ET67C - Modelagem e Controle de Sistemas Automatizados - A71 (2020_02)"',
         '"IF68E - Sistemas Embarcados - A91 (2020_02)"',
         '"ET51G - Circuitos Eletrônicos para Instrumentação - MEE11 (2021_01)"',
         '"Comunidade Moodle UTFPR"']
         for subj in subj_titles:
-            course = self.driver.find_element_by_xpath(f'//*[@title={subj}]')
-            course.click()
-            time.sleep(3)
-            self.verify_unread()
+            try:
+                course = self.driver.find_element_by_xpath(f'//*[@title={subj}]')
+                course.click()
+                time.sleep(3)
+                self.verify_unread()
+            except: 
+                None
         self.driver.quit()
  
     #Função que verifica se existem avisos não lidos e se tiver, abre eles
